bc2d2bddd358e29f6a92eccc41599f6a7508e618
CapybaraBread <137614736+CapybaraBread@users.noreply.github.com>
2024-09-08 11:37:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+import requests
+from requests.exceptions import HTTPError
+from urllib.parse import urlsplit
+import os
+import argparse
+from dotenv import load_dotenv
+load_dotenv()
+parser = argparse.ArgumentParser(description='Программа кторая сокращает ссылки, либо выводит данные этих ссылок')
+parser.add_argument('short_link', type=str, help='Введите либо короткую ссылку, либо длинную')
+args = parser.parse_args()
+
+def get_short_link(token, version, url, is_private):
+    params_short_link = {
+        'access_token':token,
+        'v':version,
+        'url':url,
+        'private':is_private
+    }
+    method = 'https://api.vk.ru/method/utils.getShortLink'
+    response = requests.get(method, params = params_short_link) 
+    response_content = response.json()
+    if 'error' in response_content:
+        raise HTTPError(response_content['error']['error_msg'])
+    else:
+        return response_content['response']['short_url']
+        
+
+def count_clicks(token, version, key, extended, interval):
+    params_short_link = {
+        'access_token':token,
+        'key':key,
+        'interval': interval,
+        'extended':extended,
+        'v':version,
+    }
+    method = 'https://api.vk.ru/method/utils.getLinkStats'
+    response = requests.get(method, params = params_short_link)
+    return response.content
+
+
+def check_link(token, version, key, extended, interval):
+    method = 'https://api.vk.ru/method/utils.getLinkStats'
+    params = {
+        'access_token':token,
+        'key':key,
+        'interval': interval,
+        'extended':extended,
+        'v':version,
+    }
+    is_short_link = False
+    response = requests.get(method, params = params)
+    if 'error' not in response.text: 
+        is_short_link = True
+    return is_short_link
+
+
+if __name__ == "__main__":
+    short_link = args.short_link
+    token = os.getenv("TELEGRAM_TOKEN")
+    version = 5.199
+    is_private = 0
+    extended = True
+    interval = 'forever'
+    key = urlsplit(short_link).path[1:]
+    is_short_link = check_link(token, version, key, extended, interval)
+    if is_short_link:
+        print(count_clicks(token, version, key, extended, interval))
+    else:
+        print(get_short_link(token, version, short_link, is_private))
+   
